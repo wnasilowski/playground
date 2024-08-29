b0aef8edd478b88c0c0cdda88b1b7ce9782cffeb
Wojciech Nasiłowski <wnasilow@gmail.com>
2024-08-29 07:25:12 +0000

    LongestCommonSubstring with memo

--- src/main/kotlin/solutions/LongestCommonSubstring.kt
+++ src/main/kotlin/solutions/LongestCommonSubstring.kt
@@ -12,22 +12,30 @@ fun run() {
 class Solution {
     lateinit private var text1: String
     lateinit private var text2: String
+    lateinit private var memo: Array<IntArray>
     fun longestCommonSubsequence(text1: String, text2: String): Int {
         if (text1.length == 0 || text2.length == 0) {
             return 0
         }
         this.text1 = text1
         this.text2 = text2
-        return recurse(0,0)
+        memo = Array(text1.length) { IntArray(text2.length) { -1 } }
+        return recurseWithMemo(0,0)
     }
-    private fun recurse(i: Int, j: Int): Int {
+    private fun recurseWithMemo(i: Int, j: Int): Int {
         if (i >= text1.length || j >= text2.length) {
             return 0
         }
-        if(text1[i] == text2[j]) {
-            return recurse(i+1, j+1)+1
-        } else {
-            return maxOf(recurse(i+1, j), recurse(i, j+1))
+        val cached = memo[i][j]
+        if(cached != -1) {
+            return cached
         }
+        val result = if (text1[i] == text2[j]) {
+            recurseWithMemo(i+1, j+1)+1
+        } else {
+            maxOf(recurseWithMemo(i+1, j), recurseWithMemo(i, j+1))
+        }
+        memo[i][j] = result
+        return result
     }
 }

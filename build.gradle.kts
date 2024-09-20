f83feec3c86fcf7fe7030a1e10dddff7ab6b7384
Wojciech Nasilowski <wojciech.nasilowski@vimn.com>
2024-09-20 19:54:10 +0200

    Pizza excersise

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,12 @@ val coroutinesVersion = "1.7.0"
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation(kotlin("test"))
+    implementation(kotlin("test"))
+    implementation(kotlin("test-junit"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    implementation("com.sksamuel.aedile:aedile-core:1.3.1") // Needed for CompanyDetailsRepository
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 }

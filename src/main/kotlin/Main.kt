c81be6121a3e2d3609d5551747e60cf9fa12b345
Wojciech Nasiłowski <wnasilow@gmail.com>
2024-12-06 20:45:56 +0000

    aoc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 package pl.wnasilowski
 
 suspend fun main() {
-  functional.collections.toparticles.main()
+  aoc.day6.main()
 }
---
+++ src/main/kotlin/aoc/day6/day6.kt
@@ -0,0 +1,17 @@
+package aoc.day6
+
+import aoc.utils.readInput
+
+fun main() {
+  
+    val input = readInput("day6/Day06")
+    println("res $input")
+    fun part1(input: List<String>): Int {
+  
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
